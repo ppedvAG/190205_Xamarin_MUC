06667691211b8f50bbc3f156d19190a2986c6744
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-05 14:06:05 +0100

    Dialoge

--- HalloForms/HalloForms/HalloForms/MainPage.xaml.cs
+++ HalloForms/HalloForms/HalloForms/MainPage.xaml.cs
@@ -13,5 +13,25 @@ namespace HalloForms
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            #region Dialogmöglichkeiten
+            // MessageBox - 1 Button
+            // DisplayAlert("Meine erste Nachricht", "Hallo Welt", "OK");
+
+            // "Ja/Nein" Messagebox
+            // DisplayAlert("Eine simple Frage", "Mittagspause ?", "Ja", "Verdammt nochmal JA !");
+
+            // Mehrfach-Frage
+            // DisplayActionSheet("Wähle dein Obst", "Ok", "Abbrechen", "Apfel", "Birne", "Banane", "Orange");
+
+            #endregion
+
+            if (string.IsNullOrWhiteSpace(entryEingabe.Text))
+                DisplayAlert("Deine Eingabe", "---LEER---", "Na geh....");
+            else
+                DisplayAlert("Deine Eingabe", entryEingabe.Text, "Ok");
+        }
     }
 }
