ceac536e896efc0e756385571409d2e10a5c1bc9
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-06 16:24:04 +0100

    Image_Demo

---
+++ Image_Demo/Image_Demo/Image_Demo/EmbeddedImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Image_Demo
+{
+    [ContentProperty(nameof(ID))]
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string ID { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (string.IsNullOrEmpty(ID))
+                return null;
+
+            return ImageSource.FromResource(ID);
+        }
+    }
+}
--- Image_Demo/Image_Demo/Image_Demo/MainPage.xaml.cs
+++ Image_Demo/Image_Demo/Image_Demo/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
 using Xamarin.Forms;
 
 namespace Image_Demo
@@ -12,6 +14,18 @@ namespace Image_Demo
         public MainPage()
         {
             InitializeComponent();
+            // Variante: Uri 
+
+            //UriImageSource source = (UriImageSource)ImageSource.FromUri(new Uri("http://www.zooroyal.de/magazin/wp-content/uploads/2017/04/hamster-760x560.jpg"));
+            //// Cache: Default True
+            //// CacheValiditiy: 24h
+            //source.CacheValidity = TimeSpan.FromHours(1);
+            //imageBild.Source = source;
+
+            // Variante: Natives Projekt
+
+            // Variante: Image im .NET Standard-Projekt
+            // imageBild.Source = ImageSource.FromResource("Image_Demo.Images.hamster.jpg");
         }
     }
 }
