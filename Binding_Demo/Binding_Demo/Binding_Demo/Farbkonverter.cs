eb7fb65bee8c778f3f01fd562f13c8c2c4bcca1e
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-06 12:04:12 +0100

    Binding inkl Konverter

---
+++ Binding_Demo/Binding_Demo/Binding_Demo/Farbkonverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Binding_Demo
+{
+    class Farbkonverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string farbe = (string)value;
+            var fieldInfo = typeof(Color).GetFields().FirstOrDefault(x => x.Name == farbe);
+
+            if (fieldInfo == null)
+                return Color.Black;
+            else
+                return fieldInfo.GetValue(typeof(Color));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
