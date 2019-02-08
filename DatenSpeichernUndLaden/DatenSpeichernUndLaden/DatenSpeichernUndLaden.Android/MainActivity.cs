eab08c142fba7a61de219ea492defc196a87f66c
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-02-08 09:00:20 +0100

    Xamarin Essentials eingebunden

--- DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden.Android/MainActivity.cs
+++ DatenSpeichernUndLaden/DatenSpeichernUndLaden/DatenSpeichernUndLaden.Android/MainActivity.cs
@@ -19,7 +19,13 @@ namespace DatenSpeichernUndLaden.Droid
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState); // add this line to your code, it may also be called: bundle
             LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
